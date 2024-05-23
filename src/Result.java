31bfa31aa134fa6ae271dd37c01c4e9c18af037f
Adhiraj Singh <adhirajsingh735@gmail.com>
2024-05-23 14:22:50 +0530

    Result class added

--- src/Result.java
+++ src/Result.java
@@ -1,9 +1,90 @@
+import java.time.LocalDate;
+
 public class Result {
     private int resultId;
-    private String student;
-    private String assignment;
-    trainer;
-    score;
-    feedback;
-    submissionDate;
+    private Students student;
+    private Assignment assignment;
+    private Trainer trainer;
+    private int score;
+    private String feedback;
+    private LocalDate submissionDate;
+
+    public Result(int resultId, Students student, Assignment assignment, Trainer trainer, int score, String feedback, LocalDate submissionDate) {
+        this.resultId = resultId;
+        this.student = student;
+        this.assignment = assignment;
+        this.trainer = trainer;
+        this.score = score;
+        this.feedback = feedback;
+        this.submissionDate = submissionDate;
+    }
+
+    public int getResultId() {
+        return resultId;
+    }
+
+    public void setResultId(int resultId) {
+        this.resultId = resultId;
+    }
+
+    public Students getStudent() {
+        return student;
+    }
+
+    public void setStudent(Students student) {
+        this.student = student;
+    }
+
+    public Assignment getAssignment() {
+        return assignment;
+    }
+
+    public void setAssignment(Assignment assignment) {
+        this.assignment = assignment;
+    }
+
+    public Trainer getTrainer() {
+        return trainer;
+    }
+
+    public void setTrainer(Trainer trainer) {
+        this.trainer = trainer;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+    public LocalDate getSubmissionDate() {
+        return submissionDate;
+    }
+
+    public void setSubmissionDate(LocalDate submissionDate) {
+        this.submissionDate = submissionDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "resultId=" + resultId +
+                ", student=" + student +
+                ", assignment=" + assignment +
+                ", trainer=" + trainer +
+                ", score=" + score +
+                ", feedback='" + feedback + '\'' +
+                ", submissionDate=" + submissionDate +
+                '}';
+    }
 }
