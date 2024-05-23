31bf3b0d4747ac67064e5fc9c6b57495bc2c5805
Adhiraj Singh <adhirajsingh735@gmail.com>
2024-05-23 16:20:04 +0530

    More data added

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
     Students s1=new Students("Adhiraj Singh",101,"adhirajsingh@gmail.com",98313941);
@@ -10,25 +13,28 @@ public class Main {
     Students s8=new Students("Akshat Jain",108,"akshatjain@gmail.com",78654912);
     Students s9=new Students("Riya Khandelwal",109,"riyakhandelwal@gmail.com",78999912);
     Students s10=new Students("Divyanshu Gautam",110,"divyanshugautam@gmail.com",76901234);
-    System.out.println(s1.toString());
-    System.out.println(s2.toString());
-    System.out.println(s3.toString());
-    System.out.println(s4.toString());
-    System.out.println(s5.toString());
-    System.out.println(s6.toString());
-    System.out.println(s7.toString());
-    System.out.println(s8.toString());
-    System.out.println(s9.toString());
-    System.out.println(s10.toString());
 
-    Trainer t1= new Trainer("Varun",1,"ad",9678956,);
-    Trainer t1= new Trainer("Varun",1,"ad",9678956,);
-    Trainer t1= new Trainer("Varun",1,"ad",9678956,);
+    TechStack ts1=new TechStack(1001,"Django","Python Framework");
+    TechStack ts2=new TechStack(1002,"Springboot","Java Framework");
+    TechStack ts3=new TechStack(1003,"ExpressJS","For Backend");
+    TechStack ts4=new TechStack(1004,"ReactJS","For Frontend");
+    TechStack ts5=new TechStack(1005,"MongoDB","For Database");
 
-    Assignment a1 = new Assignment();
-    Assignment a1 = new Assignment();
-    Assignment a1 = new Assignment();
-    Assignment a1 = new Assignment();
+    ArrayList<TechStack> techStackList = new ArrayList<>();
+    techStackList.add(ts1);
+    techStackList.add(ts2);
+    techStackList.add(ts3);
+    techStackList.add(ts4);
+    techStackList.add(ts5);
+
+    Trainer t1= new Trainer("Vanshika Ma'am",1,"vanshika@gmail.com",961111,techStackList);
+    Trainer t2= new Trainer("Ritu Ma'am",2,"ritu@gmail.com",639091,techStackList);
+    Trainer t3= new Trainer("Hridesh Sir",3,"hridesh@gmail.com",704390,techStackList);
+
+    Assignment a1 = new Assignment(10,"Arrays",);
+    Assignment a2 = new Assignment(11,"Strings",);
+    Assignment a3 = new Assignment(12,"Collection",);
+    Assignment a4 = new Assignment(13,"API's",);
 
 
 
